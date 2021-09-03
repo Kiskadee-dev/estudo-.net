a6eca16a9b624a4698fbc05287fa5c9cd6702b3e
Matheus Victor <matheusvictor870@gmail.com>
2021-09-03 16:03:44 -0300

    parte 2 - Inserir e listar alunos

--- Program.cs
+++ Program.cs
@@ -6,24 +6,36 @@ namespace projeto_basico
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe a opção desejada");
-            Console.WriteLine("1- Inserir novo aluno");
-            Console.WriteLine("2- Listar alunos");
-            Console.WriteLine("3- Calcular média geral");
-            Console.WriteLine("X- Sair");
-            Console.WriteLine();
-
-            string OpcaoUsuario = Console.ReadLine().ToUpper();
+            Aluno[] alunos = new Aluno[5];
+            int indiceAluno = 0;
+            string OpcaoUsuario = ExibirMenu();
 
             while (OpcaoUsuario != "X")
             {
                 switch(OpcaoUsuario)
                 {
                     case "1":
-                        //TODO: Inserir novo aluno
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+                        Console.WriteLine("Informe a nota do aluno: ");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal Nota))
+                        {
+                            aluno.Nota = Nota; 
+                        }else{
+                            throw new ArgumentException("O valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
                         break;
                     case "2":
-                        //TODO: Listar alunos
+                        foreach(Aluno a in alunos){
+                            if (!string.IsNullOrEmpty(a.Nome)) {
+                                Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
+                            }
+                        }
                         break;
                     case "3":
                         //TODO: Calcular média geral
@@ -31,14 +43,21 @@ namespace projeto_basico
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
-                Console.WriteLine("Informe a opção desejada");
-                Console.WriteLine("1- Inserir novo aluno");
-                Console.WriteLine("2- Listar alunos");
-                Console.WriteLine("3- Calcular média geral");
-                Console.WriteLine("X- Sair");
-                Console.WriteLine();
-                OpcaoUsuario = Console.ReadLine().ToUpper();
+                OpcaoUsuario = ExibirMenu();
             }
+        }
+
+        private static string ExibirMenu(){
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+            string input = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return input;
         }
     }
 }
