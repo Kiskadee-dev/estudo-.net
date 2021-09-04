b8c0eecb5e0e5104ea979a8bd81f35458430ba6a
Matheus Victor <matheusvictor870@gmail.com>
2021-09-04 00:55:40 -0300

    cadastro de séries: repositorio e interfaces, pt3

---
+++ cadastro_de_series/Classes/SerieRepositorio.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using cadastro_de_series.Interfaces;
+
+namespace cadastro_de_series.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
